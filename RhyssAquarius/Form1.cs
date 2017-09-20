64b5488741067d9626ffa003297d08c716f7806f
temp <temp>
2017-09-20 13:31:04 -0400

    Add project files.

---
+++ RhyssAquarius/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RhyssAquarius
+{
+    public partial class AquariusCard : Form
+    {
+        public AquariusCard()
+        {
+            InitializeComponent();
+        }
+
+        private void AquariusCard_Click(object sender, EventArgs e)
+        {
+            Graphics aquariusCard = this.CreateGraphics();
+            Pen aquariusPen = new Pen(Color.DodgerBlue);
+            SolidBrush aquariusBrush = new SolidBrush(Color.DodgerBlue);
+            aquariusCard.DrawRectangle(aquariusPen, 0, 0, 910, 175);
+            aquariusCard.FillRectangle(aquariusBrush, 0, 0, 910, 175);
+            aquariusPen.Color = Color.Gray;
+            aquariusBrush.Color = Color.Gray;
+            aquariusCard.DrawRectangle(aquariusPen, 0, 150, 910, 175);
+            aquariusCard.FillRectangle(aquariusBrush, 0, 150, 910, 175);
+            aquariusPen.Color = Color.Chartreuse;
+            aquariusBrush.Color = Color.Chartreuse;
+            aquariusCard.DrawRectangle(aquariusPen, 0, 300, 910, 175);
+            aquariusCard.FillRectangle(aquariusBrush, 0, , 910, 175);
+
+
+        }
+    }
+}
