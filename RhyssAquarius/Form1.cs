a2993860f597766eab1a04137feeafb3148ef15f
temp <temp>
2017-09-22 13:10:03 -0400

    fixed photo, started the star

--- RhyssAquarius/Form1.cs
+++ RhyssAquarius/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Threading;
+using System.Media;
 
 namespace RhyssAquarius
 {
@@ -19,7 +20,11 @@ namespace RhyssAquarius
         }
         //Rhyss Glenfield September 20, 2017
         private void AquariusCard_Click(object sender, EventArgs e)
-        {
+        {// photo
+
+            BackgroundImage = null;
+            Refresh();
+
             //graphics object
             Graphics aquariusCard = this.CreateGraphics();
 
@@ -28,6 +33,8 @@ namespace RhyssAquarius
             SolidBrush aquariusBrush = new SolidBrush(Color.DodgerBlue);
             Font aquariusFont = new Font("Papyrus", 70, FontStyle.Bold);
             SolidBrush aquariusWord = new SolidBrush(Color.Black);
+            Pen starPen = new Pen(Color.Yellow, 10);
+            SolidBrush starBrush = new SolidBrush(Color.Yellow);
 
             //the colours of aquarius
             aquariusCard.DrawRectangle(aquariusPen, 0, 0, 910, 250);
@@ -54,9 +61,19 @@ namespace RhyssAquarius
             SolidBrush rhyssWord = new SolidBrush(Color.Black);
             aquariusCard.DrawString("Rhyss Glenfield", rhyssFont, rhyssWord, 50, 550);
             aquariusCard.DrawString("September 26, 2017", rhyssFont, rhyssWord, 50, 600);
-            this.BackgroundImage = Properties.Resources.aquariusphoto;
-            this.BackgroundImageLayout = ImageLayout.Center;
 
+            //pausing
+            
+            Thread.Sleep(50);
+            aquariusCard.Clear(Color.Black);
+
+            //Inside the card
+            aquariusCard.DrawEllipse(starPen, 100, 300, 30, 30);
+            aquariusCard.FillEllipse(starBrush, 100, 300, 30, 30);
+            aquariusCard.DrawEllipse(starPen, 400, 200, 10, 10);
+            aquariusCard.FillEllipse(starBrush, 400, 200, 10, 10);
+            aquariusCard.DrawEllipse(starPen, 430, 190, 20, 20);
+            aquariusCard.FillEllipse(starBrush, 430, 190, 20, 20);
 
         }
 
